fdff046b9754a8cbe514667b1545db6dfd473705
JohnAlbin <virtually.johnalbin@gmail.com>
2019-12-08 20:01:59 +0800

    Add ability to sort Storybook "Pages" list. #1466

--- services/ui/.storybook/config.js
+++ services/ui/.storybook/config.js
@@ -4,6 +4,7 @@ import requireContext from 'require-context.macro';
 import { addDecorator, addParameters, configure } from '@storybook/react';
 import { withA11y } from '@storybook/addon-a11y';
 import { withKnobs } from '@storybook/addon-knobs';
+import storySort from './storySort';
 import withApiConnection from './decorators/ApiConnection';
 import withGlobalStyles from './decorators/GlobalStyles';
 import lagoonTheme from './lagoonTheme';
@@ -12,37 +13,9 @@ addParameters({
   options: {
     theme: lagoonTheme,
     showRoots: true,
-    storySort: (a, b) => {
-      // Debug.
-      // console.log({a, b});
-
-      // Sort by root names, using the ordering specified in rootOrder array.
-      const rootOrder = ['Home', 'Pages', 'Components'];
-
-      // If the two stories have the same story kind, then use the default
-      // ordering, which is the order they are defined in the story file.
-      if (a[1].kind === b[1].kind) {
-        return 0;
-      }
-
-      // If the stories are in different story kinds, sort by root.
-      const aRootName = a[1].kind.split('/')[0];
-      const bRootName = b[1].kind.split('/')[0];
-      if (aRootName !== bRootName) {
-        // If a root is not found in rootOrder, its index will be -1
-        // and we will sort it last.
-        const aRootIndex = rootOrder.indexOf(aRootName);
-        const bRootIndex = rootOrder.indexOf(bRootName);
-        // If at least one of the roots is found, sort by rootOrder.
-        if (!(aRootIndex === bRootIndex === -1)) {
-          return (aRootIndex === -1 ? rootOrder.length : aRootIndex)
-            - (bRootIndex === -1 ? rootOrder.length : bRootIndex);
-        }
-      }
-
-      // Otherwise, use alphabetical order.
-      return a[1].id.localeCompare(b[1].id)
-    },
+    storySort: storySort({
+      order: ['Home', 'Pages', ['Projects', 'Project', 'Environment', 'Deployments', 'Deployment', 'Backups', 'Tasks', 'Task'], 'Components']
+    }),
   },
   a11y: {
     restoreScroll: true,
